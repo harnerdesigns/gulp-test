81f20df09b1f5a8ba1743da5511651b2b91212c8
Jack Harner <jack@harnerdesigns.com>
2019-01-15 09:35:20 -0700

    default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,15 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     package = require('./package.json');
+
+
+    gulp.task( 'default', function(){
+    	console.log();
+    	console.log("====================");
+
+    	console.log("Welcome To Gulp-Test");
+    	console.log("====================");
+    	console.log("v" + package.version);
+    	console.log();
+    	
+    });
